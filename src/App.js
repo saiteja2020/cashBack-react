a2c0f2623f381d23796763bc8637a1fe3d1c6918
Sai Teja Kondamudi <saiteja2020@gmail.com>
2020-01-16 18:36:47 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+import Percentages from "./Percentages";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Percentages</h1>
+      <Percentages />
+    </div>
+  );
+}
